e083da662787a35bfa020c3e3c787b5f628404f7
mak1323 <myleskaeding@gmail.com>
2017-08-29 14:17:47 -0400

    Trying to get function to work
    Get all logs.
    
    Back end sends the logs but the front end isn't doing anything with them

--- app/router.js
+++ app/router.js
@@ -13,6 +13,8 @@ Router.map(function () {
   this.route('campaigns');
   this.route('campaign', { path: '/campaigns/:campaign_id'});
   this.route('edit-campaign', { path: '/campaigns/:campaign_id/edit-name'});
+  this.route('logs', { path: '/campaigns/:campaign_id/logs'});
+  this.route('log', { path: '/campaigns/:campaign_id/logs/:campaign_log_id'});
 });
 
 export default Router;
