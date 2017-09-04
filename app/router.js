e5cbf52eb080ff7ce5f7215770275bde78166a7b
mak1323 <myleskaeding@gmail.com>
2017-09-04 13:26:22 -0400

    Removed Everything excess ember routes and adapters
    Ran ember destroy adapter campaign
    Ran ember destroy route nouns
    Ran ember destroy route adjectives
    
    Tested to see if worked still, and it does.

--- app/router.js
+++ app/router.js
@@ -16,8 +16,6 @@ Router.map(function () {
   this.route('edit-campaign', { path: '/campaigns/:campaign_id/edit-name'});
   this.route('logs', { path: '/campaigns/logs/:campaign_id'});
   this.route('log', { path: '/campaigns/logs/:campaign_id/:campaign_log_id'});
-  this.route('nouns');
-  this.route('adjectives');
 });
 
 export default Router;
--- app/routes/nouns.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  // model () {
-  //   const writerApi =  'https://pure-tundra-23985.herokuapp.com/';
-  //   return Ember.$.ajax({ url: writerApi + 'nouns', dataType: 'json', type: 'GET' })
-  // }
-});
